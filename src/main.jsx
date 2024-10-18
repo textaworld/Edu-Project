64f3d5cc6e4c25730e333ef641e1047ba83dcd2d
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2024-10-18 14:59:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./App.tsx";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SiteDetailsContextProvider } from "./context/SiteDetailsContext.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
